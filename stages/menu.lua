93324fec0812fe842af4f7e027ad947877a19da0
arandomguyhi <theorda52@gmail.com>
2024-10-11 13:50:35 -0300

    changed to custom substate

--- stages/menu.lua
+++ stages/menu.lua
@@ -5,141 +5,171 @@ local callbacks = {}
 
 luaDebugMode = true
 function onCreate()
-    makeLuaSprite('tunnelBG', 'newMain/subway_bg_2', 0, -12)
-    setObjectCamera('tunnelBG', 'other')
-    addLuaSprite('tunnelBG')
+    setProperty('skipCountdown', true)
+    setProperty('camGame.visible', false)
+    setProperty('camHUD.visible', false) -- i don't need these cams for now
+end
 
-    makeAnimatedLuaSprite('train', 'newMain/trainmenu', 255, 140)
-    addAnimationByPrefix('train', 'come', 'Train come', 24, false)
-    addAnimationByPrefix('train', 'idle', 'Train notselected', 24, false)
-    addAnimationByPrefix('train', 'selected', 'Train selected', 24, false)
-    addOffset('train', 'idle', 0, 0)
-    addOffset('train', 'selected', 0, 42)
-    playAnim('train', 'idle')
-    scaleObject('train', 1.32, 1.32, false)
-    setObjectCamera('train', 'other')
-    addLuaSprite('train')
-    createHitbox('trainhit', 480, 205, 165, 280, function()
-	debugPrint('FUNCIONOUYAYAAAYAYA')
-    end)
+function onSongStart()
+    openCustomSubstate('Main Menu', true)
+end
 
-    makeLuaSprite('mainBG', 'newMain/subway_bg', 0, -12)
-    setObjectCamera('mainBG', 'other')
-    addLuaSprite('mainBG')
+function onCustomSubstateCreate(name)
+    if name == 'Main Menu' then
+	makeLuaSprite('tunnelBG', 'newMain/subway_bg_2', 0, -12)
+	setObjectCamera('tunnelBG', 'other')
+	addLuaSprite('tunnelBG')
 
-    makeAnimatedLuaSprite('options', 'newMain/options', 915.5, 580.55)
-    addAnimationByPrefix('options', 'idle', 'options notselected', 24, false)
-    addAnimationByPrefix('options', 'selected', 'options selected', 24, false)
-    addOffset('options', 'idle', 0, 0)
-    addOffset('options', 'selected', 0, 34)
-    playAnim('options', 'idle')
-    setObjectCamera('options', 'other')
-    addLuaSprite('options')
-    createHitbox('optionshit', 915.5, 580.55, 365, 105, function()
-	debugPrint('diferente')
-    end)
+	makeAnimatedLuaSprite('train', 'newMain/trainmenu', 255, 140)
+	addAnimationByPrefix('train', 'come', 'Train come', 24, false)
+	addAnimationByPrefix('train', 'idle', 'Train notselected', 24, true)
+	addAnimationByPrefix('train', 'selected', 'Train selected', 24, false)
+	addOffset('train', 'idle', 0, 0)
+	addOffset('train', 'selected', 0, 42)
+	playAnim('train', 'idle')
+	scaleObject('train', 1.32, 1.32, false)
+	setObjectCamera('train', 'other')
+	addLuaSprite('train')
+	createHitbox('trainhit', 480, 205, 165, 280, function()
+	    debugPrint('FUNCIONOUYAYAAAYAYA')
+	end)
 
-    makeAnimatedLuaSprite('credits', 'newMain/credits', -10, 45)
-    addAnimationByPrefix('credits', 'idle', 'credits notselected', 24, false)
-    addAnimationByPrefix('credits', 'selected', 'credits selected', 24, false)
-    addOffset('credits', 'idle', 0, 0)
-    addOffset('credits', 'selected', 216, 172)
-    playAnim('credits', 'idle')
-    setObjectCamera('credits', 'other')
-    addLuaSprite('credits')
-    createHitbox('creditshit', 40, 175, 225, 525)
+	makeLuaSprite('mainBG', 'newMain/subway_bg', 0, -12)
+	setObjectCamera('mainBG', 'other')
+	addLuaSprite('mainBG')
 
-    makeAnimatedLuaSprite('freeplay', 'newMain/freeplay', 1100, 160)
-    addAnimationByPrefix('freeplay', 'idle', 'Freeplay not selected', 24, false)
-    addAnimationByPrefix('freeplay', 'selected', 'Freeplay selected', 24, false)
-    addOffset('freeplay', 'idle', 0, 0)
-    addOffset('freeplay', 'selected', 256, 170)
-    playAnim('freeplay', 'idle')
-    setObjectCamera('freeplay', 'other')
-    addLuaSprite('freeplay')
-    createHitbox('freeplayhit', 1100, 160, 145, 225)
+	makeAnimatedLuaSprite('options', 'newMain/options', 915.5, 580.55)
+	addAnimationByPrefix('options', 'idle', 'options notselected', 24, true)
+	addAnimationByPrefix('options', 'selected', 'options selected', 24, false)
+	addOffset('options', 'idle', 0, 0)
+	addOffset('options', 'selected', 0, 34)
+	playAnim('options', 'idle')
+	setObjectCamera('options', 'other')
+	addLuaSprite('options')
+	createHitbox('optionshit', 915.5, 580.55, 365, 105, function()
+	    startTween('zoomOption', 'camOther', {y = -60}, 2, {})
+	    cameraFade('camOther', '000000', 0.5, false)
+	    runTimer('switchToOptions', 0.5)
+	    function onTimerCompleted(tag)
+		if tag == 'switchToOptions' then debugPrint('switched') end
+	    end
+	end)
 
-    makeAnimatedLuaSprite('boombox', 'newMain/boombox', 779, 433)
-    addAnimationByPrefix('boombox', 'idle', 'boombox not selected', 24, false)
-    addAnimationByPrefix('boombox', 'selected', 'boombox selected', 24, false)
-    addOffset('boombox', 'idle', 0, 0)
-    addOffset('boombox', 'selected', 0, 5)
-    playAnim('boombox', 'idle')
-    setObjectCamera('boombox', 'other')
-    addLuaSprite('boombox')
-    createHitbox('boomboxhit', 779, 433, 165, 135)
+	makeAnimatedLuaSprite('credits', 'newMain/credits', -10, 45)
+	addAnimationByPrefix('credits', 'idle', 'credits notselected', 24, true)
+	addAnimationByPrefix('credits', 'selected', 'credits selected', 24, false)
+	addOffset('credits', 'idle', 0, 0)
+	addOffset('credits', 'selected', 216, 172)
+	playAnim('credits', 'idle')
+	setObjectCamera('credits', 'other')
+	addLuaSprite('credits')
+	createHitbox('creditshit', 40, 175, 225, 525)
 
-    makeAnimatedLuaSprite('costumes', 'newMain/costumes', 505, 580)
-    addAnimationByPrefix('costumes', 'idle', 'costume notselected', 24, false)
-    addAnimationByPrefix('costumes', 'selected', 'costume selected', 24, false)
-    addOffset('costumes', 'idle', 0, 0)
-    addOffset('costumes', 'selected', 83, 102)
-    playAnim('costumes', 'idle')
-    setObjectCamera('costumes', 'other')
-    addLuaSprite('costumes')
-    createHitbox('costumeshit', 505, 580, 240, 115)
+	makeAnimatedLuaSprite('freeplay', 'newMain/freeplay', 1100, 160)
+	addAnimationByPrefix('freeplay', 'idle', 'Freeplay not selected', 24, true)
+	addAnimationByPrefix('freeplay', 'selected', 'Freeplay selected', 24, false)
+	addOffset('freeplay', 'idle', 0, 0)
+	addOffset('freeplay', 'selected', 256, 170)
+	playAnim('freeplay', 'idle')
+	setObjectCamera('freeplay', 'other')
+	addLuaSprite('freeplay')
+	createHitbox('freeplayhit', 1100, 160, 145, 225)
 
-    makeAnimatedLuaSprite('extras', 'newMain/extra', 839, 210)
-    addAnimationByPrefix('extras', 'idle', 'extras notselected', 24, false)
-    addAnimationByPrefix('extras', 'selected', 'extras selected', 24, false)
-    addOffset('extras', 'idle', 0, 0)
-    addOffset('extras', 'selected', 258, 258)
-    playAnim('extras', 'idle')
-    setObjectCamera('extras', 'other')
-    addLuaSprite('extras')
+	makeAnimatedLuaSprite('boombox', 'newMain/boombox', 779, 433)
+	addAnimationByPrefix('boombox', 'idle', 'boombox not selected', 24, true)
+	addAnimationByPrefix('boombox', 'selected', 'boombox selected', 24, false)
+	addOffset('boombox', 'idle', 0, 0)
+	addOffset('boombox', 'selected', 0, 5)
+	playAnim('boombox', 'idle')
+	setObjectCamera('boombox', 'other')
+	addLuaSprite('boombox')
+	createHitbox('boomboxhit', 779, 433, 165, 135)
 
-    createHitbox('extrashit', 839, 210, 150, 175)
+	makeAnimatedLuaSprite('costumes', 'newMain/costumes', 505, 580)
+	addAnimationByPrefix('costumes', 'idle', 'costume notselected', 24, true)
+	addAnimationByPrefix('costumes', 'selected', 'costume selected', 24, false)
+	addOffset('costumes', 'idle', 0, 0)
+	addOffset('costumes', 'selected', 83, 102)
+	playAnim('costumes', 'idle')
+	setObjectCamera('costumes', 'other')
+	addLuaSprite('costumes')
+	createHitbox('costumeshit', 505, 580, 240, 115)
 
-    makeLuaText('versionShit', 'Friday Night Fever 0.0.1', 0, 0, 0)
-    setTextSize('versionShit', 20)
-    setTextFont('versionShit', 'Plunge.ttf')
-    setTextAlignment('versionShit', 'left')
-    setProperty('versionShit.x', screenWidth - getProperty('versionShit.width') - 10)
-    setProperty('versionShit.y', screenHeight - getProperty('versionShit.height') - 10)
-    setObjectCamera('versionShit', 'other')
-    addLuaText('versionShit')
+	makeAnimatedLuaSprite('extras', 'newMain/extra', 839, 210)
+	addAnimationByPrefix('extras', 'idle', 'extras notselected', 24, true)
+	addAnimationByPrefix('extras', 'selected', 'extras selected', 24, false)
+	addOffset('extras', 'idle', 0, 0)
+	addOffset('extras', 'selected', 258, 258)
+	playAnim('extras', 'idle')
+	setObjectCamera('extras', 'other')
+	addLuaSprite('extras')
 
-    makeAnimatedLuaSprite('hand', 'newMain/cursor')
-    addAnimationByPrefix('hand', 'idle', 'cursor nonselect', 0)
-    addAnimationByPrefix('hand', 'select', 'cursor select', 0)
-    addAnimationByPrefix('hand', 'qidle', 'cursor qnonselect', 0)
-    addAnimationByPrefix('hand', 'qselect', 'cursor qselect', 0)
-    playAnim('hand', 'select')
-    setGraphicSize('hand', getProperty('hand.width') / 1.5, getProperty('hand.height') / 1.5)
-    setProperty('hand.antialiasing', true)
-    setObjectCamera('hand', 'other')
-    addLuaSprite('hand', true)
+	createHitbox('extrashit', 839, 210, 150, 175)
+
+	makeLuaText('versionShit', 'Friday Night Fever 0.0.1', 0, 0, 0)
+	setTextSize('versionShit', 20)
+	setTextFont('versionShit', 'Plunge.otf')
+	setTextAlignment('versionShit', 'left')
+	setProperty('versionShit.x', screenWidth - getProperty('versionShit.width') - 10)
+	setProperty('versionShit.y', screenHeight - getProperty('versionShit.height') - 10)
+	setObjectCamera('versionShit', 'other')
+	addLuaText('versionShit', true)
+
+	makeAnimatedLuaSprite('hand', 'newMain/cursor')
+	addAnimationByPrefix('hand', 'idle', 'cursor nonselect', 0)
+	addAnimationByPrefix('hand', 'select', 'cursor select', 0)
+	addAnimationByPrefix('hand', 'qidle', 'cursor qnonselect', 0)
+	addAnimationByPrefix('hand', 'qselect', 'cursor qselect', 0)
+	playAnim('hand', 'idle')
+	setGraphicSize('hand', getProperty('hand.width') / 1.5, getProperty('hand.height') / 1.5)
+	setProperty('hand.antialiasing', true)
+	setObjectCamera('hand', 'other')
+	addLuaSprite('hand', true)
+
+	playMusic('freakyMenu', 1, true)
+    end
 end
 
 local playedAnim, playedAnimSel = false -- this is probably so dumb
-function onUpdate()
+function onCustomSubstateUpdate(name)
     setProperty('hand.x', getMouseX('other')) setProperty('hand.y', getMouseY('other'))
 
-    for _, entry in ipairs(callbacks) do
-	if mouseOverlaps(entry.sprite) then
-	    playAnim(entry.sprite:gsub('hit', ''), 'selected')
-	    if mouseClicked() then
-		if entry.callback then -- check if it exists (just for now)
-		entry.callback()
-		end
-	    end
-	end
-    end
-
---[[    if mousePressed() then
+    
+    if mousePressed() then
 	playAnim('hand', selectedSomethin and 'qselect' or 'select')
 	setProperty('hand.offset.y', selectedSomethin and 34 or 8)
     else
 	playAnim('hand', selectedSomethin and 'qidle' or 'idle')
 	setProperty('hand.offset.y', selectedSomethin and 24 or 0)
-    end]]
+    end
+
+    if name == 'Main Menu' then
+ 	if keyboardJustPressed('ENTER') then
+	    closeCustomSubstate()
+	end
+
+	for _, entry in ipairs(callbacks) do
+	    if mouseOverlaps(entry.sprite) then
+		selectedSomethin = true
+		playAnim(entry.sprite:gsub('hit', ''), 'selected', true)
+		if mouseClicked() then
+		    if entry.callback then -- check if it exists (just for now)
+		    entry.callback()
+		    end
+		end
+	    else
+		selectedSomethin = false
+		playAnim(entry.sprite:gsub('hit', ''), 'idle', true)
+	    end
+	end
+    end
 end
 
 function createHitbox(tag, xpos, ypos, width, height, cb)
     makeLuaSprite(tag, nil, xpos, ypos)
     makeGraphic(tag, width, height, 'ffffff')
     setObjectCamera(tag, 'other')
-    setProperty(tag..'.visible', false)
+    setProperty(tag..'.alpha', 0.001)
     addLuaSprite(tag)
     table.insert(hitboxes, tag)
 
@@ -147,7 +177,5 @@ function createHitbox(tag, xpos, ypos, width, height, cb)
 end
 
 function mouseOverlaps(spr)
-    -- much easier :)
-    --return runHaxeCode("FlxG.mouse.overlaps(game.getLuaObject('"..spr.."'));")
     return getMouseX('other') > getProperty(spr..'.x') and getMouseX('other') < getProperty(spr..'.x') + getProperty(spr..'.width') and getMouseY('other') > getProperty(spr..'.y') and getMouseY('other') < getProperty(spr..'.y') + getProperty(spr..'.height')
 end
